f16991d04e9aa9c583b71057927d4b8b1c994aaa
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-01-16 11:10:09 +0200

    Update 20210115_18_1.c

--- Literals/20210115_18_1.c
+++ Literals/20210115_18_1.c
@@ -1,26 +1,27 @@
+// продължение на задачата с if - else
 #include <stdio.h>
 
 int main()
 {
     int numCaravans = 3;
     int numCampers = 3;
-    double priceCaravan = 90;
-    double priceCamper = 100;
+    int priceCaravan = 90;
+    int priceCamper = 100;
 
-    printf("Welcome to Caravan Camper for Rent\n");
-    printf("For a Caravan, press 1\n");
-    printf("For a Camper, press 2\n");
+    printf("Добре дошли в каравани и кемпери под наем\n");
+    printf("Ако желаете каравана, натиснете 1\n");
+    printf("Ако желаете кемпер, натиснете 2\n");
 
     char rez;
     scanf("%c", &rez);
 
-    double total;
+    int total;
     if (rez == '1'){
         total += 90;
     } else {
         total += 100;
     }
-    printf("You have to pay total %.2lf leva.\n", total);
-    printf("Would you like anything else?");
+    printf("ТРябва да платите общо %d leva.\n", total);
+    printf("Желаете ли да продължите?");
     return 0;
 }
