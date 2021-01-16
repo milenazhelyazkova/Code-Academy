5537d50c9c1705a3748064685ae59afaa2448642
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-01-16 11:48:15 +0200

    Update 20210115_7.c

--- Literals/20210115_7.c
+++ Literals/20210115_7.c
@@ -4,7 +4,7 @@ int main(void){
     double a = 0xBB;
     signed long long b = 0xBB;
     unsigned long long c = 0xBBu;
-    printf("a = %lf, b = %lld, c = %llu", a, b, c);
+    printf("a = %lf, b = %lld, c = %lld", a, b, c);
 
     return 0;
 }
