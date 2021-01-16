11ce200ab3d0028033cba824ec7a13dc87650422
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-01-16 11:20:31 +0200

    Update 20210115_11_1.c

--- Literals/20210115_11_1.c
+++ Literals/20210115_11_1.c
@@ -2,13 +2,13 @@
 
 int main(){
     char str[] = "Hello";
-    char rts[] = "Hello";
-  
-    rts[4] = str[0];
-    rts[3] = str[1];
-    rts[2] = str[2];
-    rts[1] = str[3];
-    rts[0] = str[4];
+    char rts[5];
+    rst[5] = str[0]
+    rts[4] = str[1];
+    rts[3] = str[2];
+    rts[2] = str[3];
+    rts[1] = str[4];
+    rts[0] = str[5];
     
    
     printf("%s",rts);
