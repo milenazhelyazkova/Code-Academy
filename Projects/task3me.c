f700e4eddf6057521707ca73ce881322bc7c56ea
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-01-28 12:23:06 +0200

    Update task3me.c

--- Projects/task3me.c
+++ Projects/task3me.c
@@ -1,11 +1,12 @@
-/*Създайте програма, която подканя човека да въведе проста аритметична операция с две числа от екрана
-- събиране, изваждане, деление, умножение и отпечатва резултата.
+/*Създайте програма, която подканя човека да въведе проста аритметична операция с две числа от екрана 
+- събиране, изваждане, деление, умножение и отпечатва резултата. 
 Помислете какви променливи и колко трябва да имате?
 Колко са вариантите, които можете да имате, като операции,
 има ли изключителни случаи, за които трябва да помислим (напишете си ги)?
 Какво ще използвате за управление на процеса ?*/
+
 #include <stdio.h>
- 
+
 int main(){
     float a;
     float b;
@@ -15,17 +16,17 @@ int main(){
         scanf("%f", &a);
         scanf("%f", &b);
         printf("a = %.2f\nb = %.2f\n", a , b);
- 
+
         printf("If You want to sum a + b press 1.\n");
         printf("If You want to subtrack a - b press 2.\n");
         printf("If You want to multiply a * b press 3.\n");
         printf("If You want to divide a / b press 4.\n");
         printf("If You want to exit press 5.\n");
- 
+
         while (choice != 1 || choice != 2 || choice != 3 || choice !=4 || choice !=5){
             scanf("%d", &choice);
             float result = 0.0;
- 
+
             if (choice == 1){
                 result = a + b;
                 printf("a+b: %.2f + %.2f = %.2f\n",a, b, result);
@@ -63,9 +64,7 @@ int main(){
         }
         if (choice == 5){
             break;
-        }      
+        }       
     }
     return 0;  
 }
- 
-Reply to "Unti
