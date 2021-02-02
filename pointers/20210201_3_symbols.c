41994bebe649f598cb622d97044cf49add107280
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-02-02 11:19:42 +0200

    Update 20210201_3_symbols.c

--- pointers/20210201_3_symbols.c
+++ pointers/20210201_3_symbols.c
@@ -18,7 +18,7 @@ double atof(char s[]){
     int i = 0;
     double power = 1.0;
     int sign = 1;
-    while (!(s[i] >= '0' && s[i] <= '9')){ /* checks the i symbol, if the symbol is not a digit, checks the next one */
+    while (!(s[i] >= '0' && s[i] <= '9')){ /* checks the i-th symbol, if the symbol is not a digit, checks the next one */
         i++;
     }
     if(i>0 && s[i-1] == '-'){ /* if there were symbols before the first digit, checks the last simbol before the  first digit. if it is - , the sign is -1 */
