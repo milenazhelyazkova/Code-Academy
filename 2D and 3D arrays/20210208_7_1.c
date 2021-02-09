28c84181b50f3aba0804479dc29e705fdfdba459
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-02-09 02:18:15 +0200

    Update 20210208_7_1.c

--- 2D and 3D arrays/20210208_7_1.c
+++ 2D and 3D arrays/20210208_7_1.c
@@ -8,7 +8,8 @@
 
 #include <stdio.h>
 
-int checkDay(int days[], int year, int month, int day);
+extern int days[12];
+int checkDay(int year, int month, int day);
 void check();
 
 int main(){
@@ -19,7 +20,6 @@ int main(){
 } 
 void check(){
    int year, month, day;
-   int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
    printf("Please enter an year you want to check: ");
    scanf("%d", &year);
    printf("\nPleace enter a month(as a number from 1 to 12): ");
@@ -31,19 +31,19 @@ void check(){
    } 
    printf("\nPleace enter a day: ");
    scanf("%d", &day);
-  if(month == 1 || month == 3 || month == 5 || month == 7 || month==8 || month==10 || month==12)
+  if(month == 1 || month == 3 || month == 5 || month == 7 || month==8 || month==10 || month==12){
       if(day<1 || day>31){
          printf("the day shouldbe between 1 and 31");
          printf("\nPleace enter a day: ");
          scanf("%d", &day);
       }
-   if(month == 4 || month==6 || month==9 ||month==11)
+   }else if(month == 4 || month==6 || month==9 ||month==11){
        if(day<1 || day>30){
          printf("the day shouldbe between 1 and 30");
          printf("\nPleace enter a day: ");
          scanf("%d", &day);
        }
-   if(month==2){
+   }else if(month==2){
       if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
          if(day<1 || day>29){
             printf("the day shouldbe between 1 and 29");
@@ -58,13 +58,14 @@ void check(){
             scanf("%d", &day);
          }
    }
-printf("\n%dth/%d/%d, is the %dth day of the year", day, month, year, checkDay(days,year,month,day));
+printf("\n%dth/%d/%d, is the %dth day of the year", day, month, year, checkDay(year,month,day));
 }
-int checkDay(int days[], int year, int month, int day){
-   if (month>2 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-         ++day;
+int checkDay(int year, int month, int day){
+   if (month>2 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) /* if month is after february and the year is high */
+         ++day;  /* add 1 day, for 29th of february */
    while (month-- > 0)
         day += days[month - 1]; 
    return day; 
 } 
+int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
   
