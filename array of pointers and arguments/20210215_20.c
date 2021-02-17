2bb544d137be6b5b2ad343d682637a4234939131
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-02-17 09:02:31 +0200

    Update 20210215_20.c

--- array of pointers and arguments/20210215_20.c
+++ array of pointers and arguments/20210215_20.c
@@ -1,39 +1,18 @@
-/*
-Задача 20. Дефинирайте стринг „I am a poor programmer”.
-Заменете през пойнтер “poor” с “great”.
+/* 
+Дефинирайте стринг „I am a poor programmer”.
+Заменете през пойнтер “poor” с “great”
 */
-
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-void replaceWordInText(const char *text, const char *oldWord, const char *newWord) {
-   int i = 0, cnt = 0;
-   int len1 = strlen(newWord);
-   int len2 = strlen(oldWord);
-   for (i = 0; text[i] != '\0'; i++) {
-      if (strstr(&text[i], oldWord) == &text[i]) {
-         cnt++;
-         i += len2 - 1;
-      }
-   }
-   char *newString = (char *)malloc(i + cnt * (len1 - len2) + 1);
-   i = 0;
-   while (*text) {
-      if (strstr(text, oldWord) == text) {
-         strcpy(&newString[i], newWord);
-         i += len1;
-         text += len2;
-      }
-      else
-      newString[i++] = *text++;
-   }
-   printf("New String: %s\n", newString);
-}
-int main() {
-   char str[] = "I ama a poor programming";
-   char c[] = "poor";
-   char d[] = "great";
-   printf("Original string: %s\n", str);
-   replaceWordInText(str, c, d);
-   return 0;
+
+int main(){
+    char str1[] = "I am a poor  programmer."; /* added extra space after "poor" (4 chars) */
+    char str2[] = "great"; /* 5 chars */
+    char *p1 = &str1[7];
+    char *p2 = str2;
+    while (*p2){
+    *p1++ = *p2++;
+    }
+
+    printf("%s\n", str1);
+    return 0;
 }
