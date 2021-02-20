5c75237d3d535ddeab4d067a9d8a575ccc041839
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-02-20 17:22:16 +0200

    Update 20210219_15.c

--- 19. structures/20210219_15.c
+++ 19. structures/20210219_15.c
@@ -9,31 +9,43 @@ tagTMyTime. Напишете две функции, които изваждат 
 променливи от тип struct tagMyTime и връщат променлива от същия тип.
 Използвайте тези функции, за да се уверите, че работят коректно.
 */
+/*
+Задача 15.Опишете времето: часове:минути:секунди като структура
+tagTMyTime. Използвайки тази структура, дефинирайте следните функции:
+добавяне на секунди, добавяне на минути. добавяне на часове към дадена
+променлива от тип struct tagTMyTime. Напишете следните функции:
+връщане на броя секунди за дадена променлива от въведения
+тип и обратна функция от секундите да се генерира променлива
+tagTMyTime. Напишете две функции, които изваждат и събират две
+променливи от тип struct tagMyTime и връщат променлива от същия тип.
+Използвайте тези функции, за да се уверите, че работят коректно.
+*/
 struct time{
-    int sec;
-    int min;
     int hour;
+    int min;
+    int sec;
 };
-struct time addsec(struct time, int sec, int min, int hour, int addsec){
-    struct time current = {sec+addsec,min,hour};
+struct time addsec(struct time, int hour, int min, int sec, int addsec){
+    struct time current = {hour,min,sec+addsec};
     if(current.sec >=60){
         current.min++;
         current.sec -=60;
     }
-    return time current;
+    return current;
 }
-struct time addmin(struct time, int sec, int min, int hour, int addmin){
-    struct time current = {sec,min+addmin,hour};
+struct time addmin(struct time, int hour, int min, int sec, int addmin){
+    struct time current = {hour, min+addmin, sec};
     if(current.min >=60){
         current.hour++;
         current.min -=60;
     }
-    return time current;
+    return current;
 }
-struct time addhour(struct time, int sec, int min, int hour, int addhour){
-    struct time current = {sec,min,hour+addhour};
+struct time addhour(struct time, int hour, int min, int sec, int addhour){
+    struct time current = {hour+addhour, min, sec};
     if(current.hour >=24){
         current.hour -=24;
     }
-    return time current;
+    return current;
 }
+
