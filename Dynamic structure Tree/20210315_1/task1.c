a2e778150028b27c643e697efc812c86d47f3e26
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-03-16 13:32:47 +0200

    Add files via upload

---
+++ Dynamic structure Tree/20210315_1/task1.c
@@ -0,0 +1,28 @@
+#include "task1.h"
+
+int push(int n)
+{
+    node_t *p = start;
+    start = (node_t *)malloc(sizeof(node_t));
+    if (!start)
+        return 1;
+    start->data = n;
+    start->next = p;
+    return 0;
+}
+
+int pop(int *n)
+{
+    if (start)
+    {
+        *n = start->data;
+        node_t *p = start;
+        start = start->next;
+        free(p);
+        return 1;
+    }
+    else
+    {
+        return 0; /* no more elements */
+    }
+}
