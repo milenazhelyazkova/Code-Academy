6a7bf7b9ef8d58266a6c4a59673560ca6bade6aa
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-06-10 19:42:16 +0300

    Update vzemibitna.c

--- bash/task_test/vzemibitna.c
+++ bash/task_test/vzemibitna.c
@@ -12,6 +12,11 @@ int main(int argc, char* argv[]){
 	int number = atoi(argv[1]);
 	int position = atoi(argv[2]);
 	if(position < 0){
+		printf("position must be a possitive number");
+		exit(1);
+	}
+	if(argc!=3){
+		printf("two arguments needed");
 		exit(1);
 	}
 	else{
