d1a6ced3099e6949915edef783630cbb1d533d1f
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2021-06-16 09:05:42 +0300

    Update test_task7.sh

--- bash/test_strings/test_task7.sh
+++ bash/test_strings/test_task7.sh
@@ -4,19 +4,27 @@ EXEC=task7
 SRC=task7.c
 if [ "$SRC" -nt "$EXEC" ]
 then
-        echo "Rebuilding $EXEC..."
-        $CC $SRC -o $EXEC
+    echo "Rebuilding $EXEC..."
+    $CC $SRC -o $EXEC
 fi
-input=".test_cases.txt"
+input="./test_cases.txt"
 COUNTER=1
-check_output(){
-    if[ "$READ" -ne "0" ]
+check_output () {
+    if [ "$READ" -ne "0" ]
     then
-        echo -e "\nTest $COUNTER failed, exit status: $READ\n"
+        if [ "$READ" -eq "1" ]
+        then
+            READ="1 Two agruments needed"
+        elif [ "$READ" -eq "2" ]
+        then
+            READ="2 Wrong agrument format"
+        fi
+        echo -e "\nTest $COUNTER failed,argument were $line exit status: $READ\n"
     else
         echo -e "\nTest $COUNTER passed\n"
     fi
-while IFS=read -r line
+}
+while IFS= read -r line
     do
         ./$EXEC $line
         READ=$?
